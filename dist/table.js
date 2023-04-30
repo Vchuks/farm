8e520f170f400e66eceb9420a6b6f1432022cad5
Adetunji <idowuolatunde32@gmail.com>
2023-04-30 18:15:43 +0100

    pagination added

--- dist/table.js
+++ dist/table.js
@@ -121,7 +121,6 @@ function getDateRange(event) {
 }
 
 
-
 const header = new Headers();
 header.append("Authorization", `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcl90eXBlIjoiYWRtaW4iLCJpYXQiOjE2ODI3ODgwNjl9.2_3_yniURb3EGrpX50I0HplmoXsEZSN688a-s3XN8CM`)
 
@@ -153,70 +152,166 @@ arr.map(num=>{
 
 
 
+// const apiUrl = 'https://zlglobalalliance.com.ng/api/daterange-filter-medical-records?page=1';
+// const perPage = 10;
+// let currentPage = 1;
+
+// const dataContainer = document.querySelector('#data-container');
+// const prevBtn = document.querySelector('#prev-btn');
+// const nextBtn = document.querySelector('#next-btn');
+
+// function fetchData(page) {
+//   fetch(`${apiUrl}?page=${page}&per_page=${perPage}`)
+//     .then(response => response.json())
+//     .then(data => renderData(data));
+// }
+
+// function renderData(data) {
+//   // Clear the data container
+//   dataContainer.innerHTML = '';
+
+//   // Render the data
+//   data.forEach(item => {
+//     const itemElement = document.createElement('div');
+//     itemElement.classList.add('item');
+
+//     const titleElement = document.createElement('h2');
+//     titleElement.textContent = item.title;
+
+//     const bodyElement = document.createElement('p');
+//     bodyElement.textContent = item.body;
+
+//     itemElement.appendChild(titleElement);
+//     itemElement.appendChild(bodyElement);
+
+//     dataContainer.appendChild(itemElement);
+//   });
+
+//   // Enable/disable the pagination buttons
+//   if (currentPage === 1) {
+//     prevBtn.disabled = true;
+//   } else {
+//     prevBtn.disabled = false;
+//   }
+//   if (data.length < perPage) {
+//     nextBtn.disabled = true;
+//   } else {
+//     nextBtn.disabled = false;
+//   }
+// }
+
+// prevBtn.addEventListener('click', () => {
+//   if (currentPage > 1) {
+//     currentPage--;
+//     fetchData(currentPage);
+//   }
+// });
+
+// nextBtn.addEventListener('click', () => {
+//   currentPage++;
+//   fetchData(currentPage);
+// });
+
+// fetchData(currentPage);
 
 
-// SETTING PAGINATION FOR EACH PAGE
-// selecting required element
-const element = document.querySelector(".pagination ul");
-let totalPages = 1000;
-let page = 10;
 
-//calling function with passing parameters and adding inside element which is ul tag
-element.innerHTML = createPagination(totalPages, page);
-function createPagination(totalPages, page){
-  let liTag = '';
-  let active;
-  let beforePage = page - 1;
-  let afterPage = page + 1;
-  if(page > 1){ //show the next button if the page value is greater than 1
-    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
-  }
 
-  if(page > 2){ //if page value is less than 2 then add 1 after the previous button
-    liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`;
-    if(page > 3){ //if page value is greater than 3 then add this (...) after the first li or page
-      liTag += `<li class="dots"><span>...</span></li>`;
-    }
-  }
+// const header = new Headers();
+// header.append("Authorization", `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcl90eXBlIjoiYWRtaW4iLCJpYXQiOjE2ODI3ODgwNjl9.2_3_yniURb3EGrpX50I0HplmoXsEZSN688a-s3XN8CM`)
 
-  // how many pages or li show before the current li
-  if (page == totalPages) {
-    beforePage = beforePage - 2;
-  } else if (page == totalPages - 1) {
-    beforePage = beforePage - 1;
-  }
-  // how many pages or li show after the current li
-  if (page == 1) {
-    afterPage = afterPage + 2;
-  } else if (page == 2) {
-    afterPage  = afterPage + 1;
-  }
+// const reqq = {
+//     method: "GET",
+//     headers:header
+// }
+// const arr = []
+// function getPage(pagenum){
+//     fetch(`https://zlglobalalliance.com.ng/api/get-medical-records?page=${pagenum}`,reqq)
+//     .then(response=>response.json())
+//     .then(result=>{
+//         console.log(result)
+//         localStorage.setItem("pagenum",JSON.stringify(result.totalpages))
+//     })
+// }
+// getPage()
 
-  for (var plength = beforePage; plength <= afterPage; plength++) {
-    if (plength > totalPages) { //if plength is greater than totalPage length then continue
-      continue;
-    }
-    if (plength == 0) { //if plength is 0 than add +1 in plength value
-      plength = plength + 1;
-    }
-    if(page == plength){ //if page is equal to plength than assign active string in the active variable
-      active = "active";
-    }else{ //else leave empty to the active variable
-      active = "";
-    }
-    liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
-  }
+// const getPageBtn = document.querySelector(".pagebtn");
+// const getArr = JSON.parse(localStorage.getItem("pagenum"))
+// for (let i =0; i < getArr; i++){
+//     arr.push(i)
+// }
 
-  if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
-    if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
-      liTag += `<li class="dots"><span>...</span></li>`;
-    }
-    liTag += `<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
-  }
+// arr.map(num=>{
+//     getPageBtn.innerHTML+=`<button onclick="getPage(${num})">${num + 1}</button>`
+// })-->
 
-  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
-    liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
-  }
-  element.innerHTML = liTag; //add li tag inside ul tag
-  return liTag; //reurn the li tag
-}
+
+
+
+
+
+// // SETTING PAGINATION FOR EACH PAGE
+// // selecting required element
+// const element = document.querySelector(".pagination ul");
+// let totalPages = 1000;
+// let page = 10;
+
+// //calling function with passing parameters and adding inside element which is ul tag
+// element.innerHTML = createPagination(totalPages, page);
+// function createPagination(totalPages, page){
+//   let liTag = '';
+//   let active;
+//   let beforePage = page - 1;
+//   let afterPage = page + 1;
+//   if(page > 1){ //show the next button if the page value is greater than 1
+//     liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
+//   }
+
+//   if(page > 2){ //if page value is less than 2 then add 1 after the previous button
+//     liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`;
+//     if(page > 3){ //if page value is greater than 3 then add this (...) after the first li or page
+//       liTag += `<li class="dots"><span>...</span></li>`;
+//     }
+//   }
+
+//   // how many pages or li show before the current li
+//   if (page == totalPages) {
+//     beforePage = beforePage - 2;
+//   } else if (page == totalPages - 1) {
+//     beforePage = beforePage - 1;
+//   }
+//   // how many pages or li show after the current li
+//   if (page == 1) {
+//     afterPage = afterPage + 2;
+//   } else if (page == 2) {
+//     afterPage  = afterPage + 1;
+//   }
+
+//   for (var plength = beforePage; plength <= afterPage; plength++) {
+//     if (plength > totalPages) { //if plength is greater than totalPage length then continue
+//       continue;
+//     }
+//     if (plength == 0) { //if plength is 0 than add +1 in plength value
+//       plength = plength + 1;
+//     }
+//     if(page == plength){ //if page is equal to plength than assign active string in the active variable
+//       active = "active";
+//     }else{ //else leave empty to the active variable
+//       active = "";
+//     }
+//     liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
+//   }
+
+//   if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
+//     if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
+//       liTag += `<li class="dots"><span>...</span></li>`;
+//     }
+//     liTag += `<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
+//   }
+
+//   if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
+//     liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
+//   }
+//   element.innerHTML = liTag; //add li tag inside ul tag
+//   return liTag; //reurn the li tag
+// }
